fafec42fb9cb2f36444862eba413a2eaa0d3a0c3
louis pons <louis.pons@icloud.com>
2016-02-16 16:05:41 +0100

    exercice done but not sexy

--- Rails-Task-Manager/config/routes.rb
+++ Rails-Task-Manager/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+
+  root "tasks#index"
+  resources :tasks
+end
   # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  # See how alltaskss lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
@@ -53,4 +57,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
